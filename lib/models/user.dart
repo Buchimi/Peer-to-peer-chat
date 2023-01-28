e8de17d4c61440045a37b13fa84b2992dced71f6
Buchimi <stormzzy900@gmail.com>
2023-01-28 15:55:59 -0600

    fixed errors

---
+++ lib/models/user.dart
@@ -0,0 +1,5 @@
+class User {
+  User({required this.name});
+  String name;
+  
+}
---
+++ lib/pages/chat_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Chat extends StatefulWidget {
+  const Chat({super.key});
+
+  @override
+  State<Chat> createState() => _ChatState();
+}
+
+class _ChatState extends State<Chat> {
+  
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
